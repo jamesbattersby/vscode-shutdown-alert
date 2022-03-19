818aeff736d0de6c1e82af24f28673d102bb6bf2
James Battersby <44945862+jamesbattersby@users.noreply.github.com>
2022-03-19 23:19:22 +0000

    To spaces

--- src/extension.ts
+++ src/extension.ts
@@ -1,38 +1,33 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import { readFileSync } from 'fs';
 import * as vscode from 'vscode';
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	const watcher = vscode.workspace.createFileSystemWatcher(new vscode.RelativePattern(vscode.Uri.file("/home/james/test/"), '**/scheduled'));
-	watcher.onDidChange(uri => { shutdownUpdated(); });
-	watcher.onDidCreate(uri => { shutdownUpdated(); });
-	watcher.onDidDelete(uri => { vscode.window.showWarningMessage("Shutdown Cancelled"); });
+    const watcher = vscode.workspace.createFileSystemWatcher(new vscode.RelativePattern(vscode.Uri.file("/home/james/test/"), '**/scheduled'));
+    watcher.onDidChange(uri => { shutdownUpdated(); });
+    watcher.onDidCreate(uri => { shutdownUpdated(); });
+    watcher.onDidDelete(uri => { vscode.window.showWarningMessage("Shutdown Cancelled"); });
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() { }
 
 function shutdownUpdated() {
-	let theTime: String = "";
-	let mode: String = "";
-	let schedule: String = readFileSync("/home/james/test/scheduled").toString();
-	let lines: String[] = schedule.split("\n");
-	lines.forEach(line => {
-		if (line.toUpperCase().startsWith("USEC")) {
-			let ms: String = line.split("=")[1];
-			var d: Date = new Date(parseInt(ms.toString(), 10) / 1000);
-			theTime = d.toString();
-		}
-		else if (line.toUpperCase().startsWith("MODE")) {
-			mode = line.split("=")[1];
-		}
-	});
-	if (theTime !== "" && mode !== "") {
-		vscode.window.showWarningMessage(`${mode} scheduled for ${theTime}.\nUse shutdown -c to cancel.`);
-	}
+    let theTime: String = "";
+    let mode: String = "";
+    let schedule: String = readFileSync("/home/james/test/scheduled").toString();
+    let lines: String[] = schedule.split("\n");
+    lines.forEach(line => {
+        if (line.toUpperCase().startsWith("USEC")) {
+            let ms: String = line.split("=")[1];
+            var d: Date = new Date(parseInt(ms.toString(), 10) / 1000);
+            theTime = d.toString();
+        }
+        else if (line.toUpperCase().startsWith("MODE")) {
+            mode = line.split("=")[1];
+        }
+    });
+    if (theTime !== "" && mode !== "") {
+        vscode.window.showWarningMessage(`${mode} scheduled for ${theTime}.\nUse shutdown -c to cancel.`);
+    }
 }
 
 // /run/systemd/shutdown/scheduled
